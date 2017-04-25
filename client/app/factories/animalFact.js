c76402c541a6abbea27e656a9fb2bb4ba98e00a8
Luke Schuyler <lukeschuyler1@gmail.com>
2017-04-25 14:48:19 -0500

    refactored factory function and returned proper data from localhost

--- client/app/factories/animalFact.js
+++ client/app/factories/animalFact.js
@@ -2,12 +2,11 @@ app.factory('AnimalFact', function($http) {
 
   return {
     getAll: function() {
-        return new Promise((resolve, reject) =>{
-          $http.get(`http://localhost:3000/api/allAnimals`)
-            .then((data) => {
-              resolve(data.data)
-            })
-        })
+       return $http.get(`http://localhost:3000/api/animals`)
+          .then((val) => {
+            console.log('val', val)
+            return val.data
+          })
       },
     add: function(newAnimal) {
       return new Promise((resolve, reject) =>{
