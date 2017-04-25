50e40391877e6dca2b49e533761c542ff52058e8
Luke Woodward <luke.wdwrd@gmail.com>
2017-04-25 15:43:31 -0500

    created http requests from factory

--- client/app/factories/ZookeeperFact.js
+++ client/app/factories/ZookeeperFact.js
@@ -3,22 +3,24 @@ app.factory('ZookeeperFact', function($http){
 
     return {
     getAll: function() {
-        return $http.get(`http://localhost:3000/api/allZookeepers`)
-          .then((val) => {
-            return val.data
-          })
+        return $http.get(`http://localhost:3000/api/zookeepers`)
+          .then((val) =>  val.data)
+      },
+    getOne: function(id) {
+        return $http.get(`http://localhost:3000/api/zookeepers/${id}`)
+          .then((val) =>  val.data)
       },
     add: function(newZookeeper) {
-      return $http.post(`http://localhost:3000/api/addZookeeper`, newZookeeper)
-        .then((val) => {
-          return val.data.zookeepers
-        })
+      return $http.post(`http://localhost:3000/api/zookeepers`, newZookeeper)
+        .then((val) =>  val.data.zookeepers)
     },
     delete: function(id) {
-      return $http.delete(`http://localhost:3000/api/zookeeper/${id}`)
-        .then((val) => {
-          return val
-        })
+      return $http.delete(`http://localhost:3000/api/zookeepers/${id}`)
+        .then((val) =>  val)
+    },
+    patch: function(updatedZookeeper) {
+      return $http.patch(`http://localhost:3000/api/zookeepers`, updatedZookeeper)
+        .then((val) =>  val)
     }
   }
 })
