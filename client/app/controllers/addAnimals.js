5c7e0fbcbb272ad3b2b203bf1c486e46eae37ac4
Luke Schuyler <lukeschuyler1@gmail.com>
2017-04-26 10:06:07 -0500

    added zones factory, added radio inputs for animals to select zone

--- client/app/controllers/addAnimals.js
+++ client/app/controllers/addAnimals.js
@@ -1,51 +1,56 @@
-app.controller('AddAnimalCtrl', function($scope, AnimalFact, ZookeeperFact){
+app.controller('AddAnimalCtrl', function($scope, AnimalFact, ZookeeperFact, ZoneFact){
 
   AnimalFact.getAll()
     .then((animals) => {
       $scope.animals = animals
-      console.log($scope.animals)
+      // console.log($scope.animals)
     })
 
     ZookeeperFact.getAll()
     .then((zookeepers) => {
-      $scope.zookeepers = zookeepers.zookeepers;
-      $scope.$apply()
+      $scope.zookeepers = zookeepers;
     })
 
-  resetCheckboxes = (arrayOfCheckboxes) => {
-    for (var i = 0; i < arrayOfCheckboxes.length; i++) {
-      arrayOfCheckboxes[i].checked = false
-    }
-  }
-
-  $scope.addAnimal = () => {
-    let selectedzookeepers = [];
-    for (var i = 0; i < $scope.zookeepers.length; i++) {
-      if($scope.zookeepers[i].checked){
-        selectedzookeepers.push($scope.zookeepers[i])
-      }
-    }
-    console.log("checked zookeepers", selectedzookeepers)
-    $scope.newAnimal.zookeepers = selectedzookeepers;
-    AnimalFact.add($scope.newAnimal)
-    .then((data) => {})
-    $scope.newAnimal = {}
-    resetCheckboxes($scope.zookeepers)
-  }
-
-  $scope.addZookeeper = () => {
-    ZookeeperFact.add($scope.newZookeeper)
-    .then(() => { 
-      $scope.zookeepers.push($scope.newZookeeper)
-      $scope.newZookeeper = {}
-      $scope.$apply()
+    ZoneFact.getAll()
+    .then(zones => {
+      console.log(zones)
+      $scope.zones = zones
     })
-  }
 
-  $scope.deleteZookeeper = (id) => {
-    ZookeeperFact.delete(id)
-    .then(() => {
-      popPage()
-    })
-  }
+  // resetCheckboxes = (arrayOfCheckboxes) => {
+  //   for (var i = 0; i < arrayOfCheckboxes.length; i++) {
+  //     arrayOfCheckboxes[i].checked = false
+  //   }
+  // }
+
+  // $scope.addAnimal = () => {
+  //   let selectedzookeepers = [];
+  //   for (var i = 0; i < $scope.zookeepers.length; i++) {
+  //     if($scope.zookeepers[i].checked){
+  //       selectedzookeepers.push($scope.zookeepers[i])
+  //     }
+  //   }
+  //   console.log("checked zookeepers", selectedzookeepers)
+  //   $scope.newAnimal.zookeepers = selectedzookeepers;
+  //   AnimalFact.add($scope.newAnimal)
+  //   .then((data) => {})
+  //   $scope.newAnimal = {}
+  //   resetCheckboxes($scope.zookeepers)
+  // }
+
+  // $scope.addZookeeper = () => {
+  //   ZookeeperFact.add($scope.newZookeeper)
+  //   .then(() => { 
+  //     $scope.zookeepers.push($scope.newZookeeper)
+  //     $scope.newZookeeper = {}
+  //     $scope.$apply()
+  //   })
+  // }
+
+  // $scope.deleteZookeeper = (id) => {
+  //   ZookeeperFact.delete(id)
+  //   .then(() => {
+  //     popPage()
+  //   })
+  // }
 })
