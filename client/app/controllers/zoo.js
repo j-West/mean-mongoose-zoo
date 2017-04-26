bf961323e6e8b2d7343e42a661be9376303bd1a9
Luke Schuyler <lukeschuyler1@gmail.com>
2017-04-26 13:44:34 -0500

    getting lukes navbar

--- client/app/controllers/zoo.js
+++ client/app/controllers/zoo.js
@@ -1,4 +1,6 @@
-app.controller('ZooCtrl', function($scope, AnimalFact){
+app.controller('ZooCtrl', function($scope, AnimalFact, ZoneFact, ZookeeperFact){
+
+  $scope.view = 'animals'
 
   const getAnimals = () =>{
     AnimalFact.getAll()
@@ -8,23 +10,34 @@ app.controller('ZooCtrl', function($scope, AnimalFact){
       })
   }
 
+  ZoneFact.getAll()
+  .then(zones => {
+    $scope.zones = zones
+  })
+
+  ZookeeperFact.getAll()
+  .then(zookeepers => {
+    console.log(zookeepers)
+    $scope.zookeepers = zookeepers
+  })
+
   getAnimals()
 
-  $scope.remove = (id) => {
-    AnimalFact.remove(id)
-    .then(() => {
-      popPage()
-    })
-  }
+  // $scope.remove = (id) => {
+  //   AnimalFact.remove(id)
+  //   .then(() => {
+  //     popPage()
+  //   })
+  // }
 
-  $scope.save = (id, updateInfo) => {
-    AnimalFact.update(id, updateInfo)
-    .then(() => {
-      console.log("updated!!")
-      $scope.editing = false;
-      popPage()
-    })
-  }
+  // $scope.save = (id, updateInfo) => {
+  //   AnimalFact.update(id, updateInfo)
+  //   .then(() => {
+  //     console.log("updated!!")
+  //     $scope.editing = false;
+  //     popPage()
+  //   })
+  // }
 
 
 })
